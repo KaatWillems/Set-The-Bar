776c6bdffbd81ca7a95d4f5b4872a507a9b46a49
sebherrerabe <sebastianherrerac1997@gmail.com>
2022-04-05 15:04:54 +0200

    what kaat said

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ const multer = require("multer");
 const cloudinary = require("cloudinary");
 const {CloudinaryStorage} = require("multer-storage-cloudinary");
 const fileupload = require('express-fileupload')
+const dotenv = require('dotenv'); //added mongodb
+dotenv.config(); //added for mongodb
 
 //passport config:
 require('./config/passport')(passport)
 //mongoose (connected with Set the bar)
-mongoose.connect('mongodb+srv://Setthebar:Setthebar100@cluster0.pxj0m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology : true})
+mongoose.connect(process.env.DB_CONNECT,{useNewUrlParser: true, useUnifiedTopology : true})
 .then(() => console.log('connected with Setthebar DB'))
 .catch((err)=> console.log(err));
 
@@ -73,4 +75,4 @@ app.use('/reviews', require('./routes/reviews'));
 
 
 
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
