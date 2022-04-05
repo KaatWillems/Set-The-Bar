be3aadd184960bc2a74b9284a3a33f7591f1b229
Kaat Willems <97091300+KaatWillems@users.noreply.github.com>
2022-04-05 14:06:49 +0200

    now it really works. if you add a review of the bar is talso appears in the bar DB in the review array

--- routes/reviews.js
+++ routes/reviews.js
@@ -7,7 +7,7 @@ const Bar = require("../models/bar").Bar
 
 
   
-  router.post("/new", async (req, res) => {  
+  router.post("/new/:id", async (req, res) => {  
   
     try{
         // let p = await Profile.find(user_id: req.user._id) //first create a page to make profiles
@@ -20,10 +20,12 @@ const Bar = require("../models/bar").Bar
         comment : req.body.reviewcomment              
         // profile: p 
       })
-
-      //console.log(req.body, "console log req body from reviews.js")
-
       await newReview.save()
+      //console.log(req.body, "console log req body from reviews.js")
+      await Bar.findOneAndUpdate({_id: req.params.id}, {$push: {reviews: newReview._id}})
+
+
+      
       res.redirect('/dashboard')
     }catch(err){
       console.log(err)
@@ -32,7 +34,7 @@ const Bar = require("../models/bar").Bar
   
   })
 
-  router.get("/:id", async (req, res) => {
+  router.get("/new/:id", async (req, res) => {
     const reviewquery = await Bar.findById(req.params.id)
     //here we should still add populate reviews when we have reviews in the DB  (.populate.Reviews)
    
