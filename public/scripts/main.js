ae2fbbd54595e4dfbd5fabe5028e495e8bf65009
sebherrerabe <sebastianherrerac1997@gmail.com>
2022-04-05 12:04:22 +0200

    results page made

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,10 +1,14 @@
-const searchBar = document.querySelector('.set-the-bar-search-bar'),
+const searchInput = document.querySelector('.search-input'),
 locationDiv = document.querySelector('.location-general-container'),
 dashboardDiv = document.querySelector('.dashboard-container'),
 setTitle = document.querySelector('.set-the-bar-title')
 
-searchBar.addEventListener('click',() => {
+searchInput.addEventListener('focus',() => {
     locationDiv.style.display = 'flex'
     dashboardDiv.style.display = 'none'
 })
 
+searchInput.addEventListener('blur',() => {
+    locationDiv.style.display = 'none'
+    dashboardDiv.style.display = 'flex'
+})
