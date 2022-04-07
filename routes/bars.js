a59c3821c6fc3fc0d2d3e83f6aec48b8d72a3f2a
sebherrerabe <sebastianherrerac1997@gmail.com>
2022-04-07 11:07:24 +0200

    read the average ratings from db in results done

--- routes/bars.js
+++ routes/bars.js
@@ -1,104 +1,86 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const Profile = require("../models/profile").Profile;
-const Bar = require("../models/bar").Bar
+const Bar = require("../models/bar").Bar;
 const Review = require("../models/review").Review;
-const {ensureAuthenticated} = require('../config/auth')
+const { ensureAuthenticated } = require("../config/auth");
 
 //const getStars = require("index").getStars
 
 // const cloudinary = require("cloudinary");
 
-
-
 // this function is also called in index.js and should be imported (check how)
-const getStars = (bar) => { 
-    let rating = bar.ratings;
-    ratingarr = rating.split(',')
-    //console.log(ratingarr)
-    starNbr = parseInt(ratingarr[0])
-    //console.log(starNbr)
-    let finalArr = []
-    for (let i = 0; i < starNbr; i++) {
-      finalArr.push('*')
-      
-    }
-    if(ratingarr[1]){
-      finalArr.push("/")
-    } else if (!starNbr[1] && starNbr < 5) {
-  
-      finalArr.push(" ")
-      }
-  
-  
-    return finalArr
+const getStars = (entry) => {
+  let rating = entry;
+  ratingarr = rating.split(".");
+  //console.log(ratingarr)
+  starNbr = parseInt(ratingarr[0]);
+  //console.log(starNbr)
+  let finalArr = [];
+  for (let i = 0; i < starNbr; i++) {
+    finalArr.push("*");
+  }
+  if (ratingarr[1]) {
+    finalArr.push("/");
+  } else if (!starNbr[1] && starNbr < 5) {
+    finalArr.push(" ");
   }
 
-
-
+  return finalArr;
+};
 
 router.post("/search", ensureAuthenticated, async (req, res) => {
-    if(!req.user.Profile){
+  if (!req.user.Profile) {
+    let barResults = await Bar.find({ addressCity: req.body.userquery });
+    // let bars = await Bar.find({name: req.body.userquery})
 
-        bararr = []
-  
-        let bars = await Bar.find({addressCity: req.body.userquery})
-        // let bars = await Bar.find({name: req.body.userquery})
+    let barsNew = barResults.map((bar) => {
+      if (bar.averages != undefined) {
+        if (bar.averages[0] != undefined) {
+          return { ...bar._doc, stars: getStars(bar.averages[0].rating.toString()) };
+        } else {
+          return { ...bar._doc, stars: getStars("0") };
+        }
+      }
+    });
+    res.render("search", {
+      user: req.user,
+      bars: barsNew,
+    });
+  } else {
+    res.render("search", {
+      user: req.user,
+      bars: barsNew,
+    });
+  }
 
-        bars.forEach(async (bar) => {
-          bararr.push({bar: bar, barrating: getStars(bar),reviews: bar.reviews})
-//           if (bar.reviews != "") {
-//           Array.from(bar.reviews).forEach(review => {
-//             console.log(review)
-//           })
-//           }
-        })
-        res.render('search',{
-          user: req.user,
-          bars: bararr
-        });
-    } else {
-        res.render('search',{
-            user: req.user,
-            bars: bararr,
-    
-          });
-    }
+  // console.log(req.body, "from profiles.js brain")
+  // const profiles = await Profile.find({"username": {$regex: req.body.user_input}})
+  // res.send({data: profiles})
+});
 
+router.get("/search", async (req, res) => {
+  res.render("dashboard", {
+    user: req.user,
+    bars: [],
+  });
+  //console.log("testtttttttt router.get")
 
-    // console.log(req.body, "from profiles.js brain")
-    // const profiles = await Profile.find({"username": {$regex: req.body.user_input}})
-    // res.send({data: profiles})
-   
-  })
+  // console.log(req.body, "from profiles.js brain")
+  // const profiles = await Profile.find({"username": {$regex: req.body.user_input}})
+  //res.send({data: profiles})
+});
 
-  router.get("/search", async (req, res) => {
-    res.render('dashboard',{
-        user: req.user,
-        bars: [],
+router.get("/show/:id", ensureAuthenticated, async (req, res) => {
+  const barquery = await Bar.findById(req.params.id);
+  //here we should still add populate reviews when we have reviews in the DB  (.populate.Reviews)
 
-      });
-    //console.log("testtttttttt router.get")
+  //console.log(barquery)
 
+  res.render("bardetail", {
+    bar: barquery,
+    user: req.user,
+  });
+});
 
-    // console.log(req.body, "from profiles.js brain")
-    // const profiles = await Profile.find({"username": {$regex: req.body.user_input}})
-     //res.send({data: profiles})
-   
-  })
-
-  router.get("/show/:id",ensureAuthenticated,  async (req, res) => {
-    const barquery = await Bar.findById(req.params.id)
-    //here we should still add populate reviews when we have reviews in the DB  (.populate.Reviews)
-   
-    //console.log(barquery)
-
-    res.render('bardetail', {
-      bar: barquery,
-      user: req.user
-    })
-  })
-
-
-
-  module.exports  = router;
+module.exports = router;
