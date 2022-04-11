f3ab3b0c04791c5e5a0a62587eb3444547e3eff4
sebherrerabe <sebastianherrerac1997@gmail.com>
2022-04-11 11:23:25 +0200

    favbars ready to display

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Profile = require("../models/profile").Profile;
+const Bar = require("../models/bar").Bar;
 const User = require("../models/user").User;
 const cloudinary = require("cloudinary");
 const fileupload = require("express-fileupload");
@@ -13,14 +14,42 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_SECRET_KEY,
 });
 
+const getStars = (bar) => {
+  let rating = bar.ratings;
+  ratingarr = rating.split(",");
+  //console.log(ratingarr)
+  starNbr = parseInt(ratingarr[0]);
+  //console.log(starNbr)
+  let finalArr = [];
+  for (let i = 0; i < starNbr; i++) {
+    finalArr.push("*");
+  }
+  if (ratingarr[1]) {
+    finalArr.push("/");
+  } else if (!starNbr[1] && starNbr < 5) {
+    finalArr.push(" ");
+  }
+
+  return finalArr;
+};
+
 router.get("/show/:id", ensureAuthenticated, async (req, res) => {
-
-  let profileDB = await Profile.findOne({_id: req.user.profile._id});
-
+  let profileDB = await Profile.findOne({ _id: req.user.profile._id });
+  let favoriteBarsIDS = profileDB.favoritebars.map((favbar) => {
+    return favbar.toString();
+  });
+  let favoriteBars = [];
+  for (const favbar of favoriteBarsIDS) {
+    let newfavBar = await Bar.findOne({ _id: favbar });
+    newfavBar.stars = getStars(newfavBar);
+    favoriteBars.push(newfavBar);
+  }
   try {
     res.render("profile", {
       user: req.user,
-      profile: profileDB
+      profile: profileDB,
+      favbars: favoriteBars,
+      favbarsids: favoriteBarsIDS
     });
   } catch (err) {
     console.log(err);
@@ -37,7 +66,7 @@ router.use(fileupload({ useTempFiles: true }));
 // pictures routes
 router.post("/upload", ensureAuthenticated, async (req, res) => {
   try {
-   // `req.files` exists thanks to the express-fileupload middleware :
+    // `req.files` exists thanks to the express-fileupload middleware :
     const fileStr = req.files.image || "https://picsum.photos/300/600";
 
     const uploadResponse = await cloudinary.uploader.upload(
@@ -45,14 +74,12 @@ router.post("/upload", ensureAuthenticated, async (req, res) => {
       {}
     );
 
-    await Profile.findByIdAndUpdate(req.user.profile._id,
-      {
-        $set: {
-          username: req.body.username,
-          profilePic: uploadResponse.url,
-        }
-      }
-    )
+    await Profile.findByIdAndUpdate(req.user.profile._id, {
+      $set: {
+        username: req.body.username,
+        profilePic: uploadResponse.url,
+      },
+    });
 
     res.redirect(`/dashboard`);
   } catch (err) {
@@ -60,6 +87,5 @@ router.post("/upload", ensureAuthenticated, async (req, res) => {
     res.redirect("/dashboard");
   }
 });
-
 
 module.exports = router;
